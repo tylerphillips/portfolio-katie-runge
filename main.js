063da50ecfbbee09e3be325f1ff533f0964a9851
Tyler Phillips <tylerfphillips@gmail.com>
2014-02-13 22:52:00 -0500

    changes, header, added works from wordpress

--- main.js
+++ main.js
@@ -29,14 +29,14 @@ $(".fancybox").fancybox({
 });
 
 //Enable thumbnail helper and set custom options
-$(".fancybox").fancybox({
-    helpers:  {
-        thumbs : {
-            width: 50,
-            height: 50
-        }
-    }
-});
+// $(".fancybox").fancybox({
+//     helpers:  {
+//         thumbs : {
+//             width: 50,
+//             height: 50
+//         }
+//     }
+// });
 
 // $("#single_2").fancybox({
 //     	openEffect	: 'elastic',
@@ -52,64 +52,64 @@ $(".fancybox").fancybox({
 // width: 1080px;
 // height: 805px;
 
-       var $img = $('img'); // finds all image tags
+//        var $img = $('img'); // finds all image tags
 
-$img.click(function resize(e) { // bind click event to all images
-  $img.css({ // resize the image
-     height: '1080px',
-     width: '805px'
-  });
-});
-    });
+// $img.click(function resize(e) { // bind click event to all images
+//   $img.css({ // resize the image
+//      height: '1080px',
+//      width: '805px'
+//   });
+// });
+//     });
 
 
 
 
 /* Wait for DOM to be ready */
-$(function () {
-	// Store original image dimensions
-	$('.photoset-item img').each(function () {
-		var img = new Image();
-		img.src = $(this).attr('src');
-		$(this)
-      .data('org-width', img.width)
-      .data('org-height', img.height)
-      .hide()
-      .closest('.photoset-item')
-      .css({
-        "background-image": "url("+img.src+")",
-        "background-position": "center center",
-      });
-	});
+// $(function () {
+// 	// Store original image dimensions
+// 	$('.photoset-item img').each(function () {
+// 		var img = new Image();
+// 		img.src = $(this).attr('src');
+// 		$(this)
+//       .data('org-width', img.width)
+//       .data('org-height', img.height)
+//       .hide()
+//       .closest('.photoset-item')
+//       .css({
+//         "background-image": "url("+img.src+")",
+//         "background-position": "center center",
+//       });
+// 	});
 
 	// Detect resize event
-	$(window).resize(function () {
+	// $(window).resize(function () {
 		// Set photoset image size
-		$('.photoset-row').each(function () {
-			var $pi    = $(this).find('.photoset-item'),
-				  cWidth = $(this).parent('.photoset').width();
+// 		$('.photoset-row').each(function () {
+// 			var $pi    = $(this).find('.photoset-item'),
+// 				  cWidth = $(this).parent('.photoset').width();
 
-			// Generate array containing all image aspect ratios
-			var ratios = $pi.map(function () {
-				return $(this).find('img').data('org-width') / $(this).find('img').data('org-height');
-			}).get();
+// 			// Generate array containing all image aspect ratios
+// 			var ratios = $pi.map(function () {
+// 				return $(this).find('img').data('org-width') / $(this).find('img').data('org-height');
+// 			}).get();
 
-			// Get sum of widths
-			var sumRatios = 0,
-          minRatio  = Math.min.apply(Math, ratios);
-			for (var i = 0; i < $pi.length; i++) {
-				sumRatios += ratios[i]/minRatio;
-			};
+// 			// Get sum of widths
+// 			var sumRatios = 0,
+//           minRatio  = Math.min.apply(Math, ratios);
+// 			for (var i = 0; i < $pi.length; i++) {
+// 				sumRatios += ratios[i]/minRatio;
+// 			};
 
-			// Calculate dimensions
-			$pi.each(function (i) {
-				var hMargins = parseInt($(this).css('margin-left')) + parseInt($(this).css('margin-right')),
-            minWidth = cWidth/sumRatios;
-				$(this)
-					.width(Math.floor(minWidth*(ratios[i]/minRatio) - hMargins)) // Use Math.floor to correct for rounding error
-				.height(Math.floor(minWidth/minRatio));
-			});
-		});
-	}).resize();
-});
+// 			// Calculate dimensions
+// 			$pi.each(function (i) {
+// 				var hMargins = parseInt($(this).css('margin-left')) + parseInt($(this).css('margin-right')),
+//             minWidth = cWidth/sumRatios;
+// 				$(this)
+// 					.width(Math.floor(minWidth*(ratios[i]/minRatio) - hMargins)) // Use Math.floor to correct for rounding error
+// 				.height(Math.floor(minWidth/minRatio));
+// 			});
+// 		});
+// 	}).resize();
+// });
 
